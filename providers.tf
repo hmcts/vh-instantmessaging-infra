6c2e437ffa53b5456d7f3b074537d1df23131f95
lowflying <chriscm.neu@gmail.com>
2021-11-15 12:39:15 +0000

    fixed providers block

--- providers.tf
+++ providers.tf
@@ -1,7 +1,9 @@
 terraform {
-  backend "azurerm" {
-    tenant_id       = "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
-    subscription_id = "705b2731-0e0b-4df7-8630-95f157f0a347"
+  backend "azurerm" {}
+
+  required_version = ">= 1.0.4"
+  required_providers {
+    azurerm = ">=2.0.0"
   }
 
 }
